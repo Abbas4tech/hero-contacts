780838b9ba68ab30b76631bf551bc91eef2cd720
Abbas4tech <shaikhabbas54@gmail.com>
2024-10-25 19:49:46 +0530

    ✨ feat: fixed issues of uid

--- src/app/modules/dashboard/modules/contacts/guard/edit-form.guard.ts
+++ src/app/modules/dashboard/modules/contacts/guard/edit-form.guard.ts
@@ -3,7 +3,7 @@ import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { Contact, ContactsQueryParams } from '../model/contacts.model';
 import { ToastrService } from 'ngx-toastr';
-import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { BrowserStorageService } from 'src/app/services/storage.service';
 
 @Injectable()
 export class CanActivateEditForm implements CanActivate {
@@ -11,12 +11,12 @@ export class CanActivateEditForm implements CanActivate {
         private _fire: Firestore,
         private _router: Router,
         private _toastr: ToastrService,
-        private _auth: AuthService
+        private _browserStorage: BrowserStorageService
     ) {}
 
     async canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
         const id = route.queryParams['id'];
-        const userId = this._auth.user.getValue().uid;
+        const userId = this._browserStorage.get('userId');
         const mode = route.queryParams['mode'];
         if (mode === ContactsQueryParams.EDIT) {
             const res = await getDoc(doc(this._fire, userId, id)).then(
