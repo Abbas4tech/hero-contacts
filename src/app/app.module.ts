1271c78011407f94c369c45c3b061debc27d2169
Abbas4tech <shaikhabbas54@gmail.com>
2024-10-30 06:22:48 +0530

    ✨ feat: updated to latest packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { ToastContainerModule, ToastrModule } from 'ngx-toastr';
+import { ToastrModule } from 'ngx-toastr';
 import { NetworkManagerService } from './services/network-manager.service';
 import { SharedModule } from './modules/shared/shared.module';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
@@ -13,7 +13,6 @@ import { provideAuth, getAuth, GoogleAuthProvider } from '@angular/fire/auth';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { AuthService } from './modules/auth/services/auth.service';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { providePerformance, getPerformance } from '@angular/fire/performance';
 
@@ -24,19 +23,13 @@ import { providePerformance, getPerformance } from '@angular/fire/performance';
         AppRoutingModule,
         BrowserAnimationsModule,
         SharedModule,
-        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-        provideAuth(() => getAuth()),
-        provideFirestore(() => getFirestore()),
-        AngularFirestoreModule.enablePersistence(),
-        provideStorage(() => getStorage()),
-        providePerformance(() => getPerformance()),
+
         ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: true,
             // Register the ServiceWorker as soon as the application is stable
             // or after 30 seconds (whichever comes first).
             registrationStrategy: 'registerWhenStable:30000',
         }),
-        ToastContainerModule,
         ToastrModule.forRoot({
             timeOut: 5000,
             progressBar: true,
@@ -50,6 +43,11 @@ import { providePerformance, getPerformance } from '@angular/fire/performance';
         AuthService,
         GoogleAuthProvider,
         { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideStorage(() => getStorage()),
+        providePerformance(() => getPerformance()),
     ],
     bootstrap: [AppComponent],
 })
